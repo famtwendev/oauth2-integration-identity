940e362ddcb039c1ce053ea0f0f033cc93571798
Famtwen <phamtuyen121314@gmail.com>
2025-02-19 12:19:37 +0700

    Only commit

---
+++ web-app/src/configurations/configuration.js
@@ -0,0 +1,5 @@
+export const OAuthConfig = {
+  clientId: "63026501275-f81ge979kgccjc0jk7nqmbrvbcbeqe0s.apps.googleusercontent.com",
+  redirectUri: "http://localhost:3000/authenticate",
+  authUri: "https://accounts.google.com/o/oauth2/auth",
+};
